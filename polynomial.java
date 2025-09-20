40119212204e9172cb7f475a0bcffe089c693972
Tanuja2024 <155653899+Tanuja2024@users.noreply.github.com>
2025-09-20 13:12:10 +0530

    Update polynomial.java

--- polynomial.java
+++ polynomial.java
@@ -1,58 +1,23 @@
 import java.util.*;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 class Main {
     public static void main(String[] args) {
 
         List<List<String>> points = new ArrayList<>();  
-        String jsonString = """
-{
-"keys": {
-    "n": 10,
-    "k": 7
-  },
-  "1": {
-    "base": "6",
-    "value": "13444211440455345511"
-  },
-  "2": {
-    "base": "15",
-    "value": "aed7015a346d635"
-  },
-  "3": {
-    "base": "15",
-    "value": "6aeeb69631c227c"
-  },
-  "4": {
-    "base": "16",
-    "value": "e1b5e05623d881f"
-  },
-  "5": {
-    "base": "8",
-    "value": "316034514573652620673"
-  },
-  "6": {
-    "base": "3",
-    "value": "2122212201122002221120200210011020220200"
-  },
-  "7": {
-    "base": "3",
-    "value": "20120221122211000100210021102001201112121"
-  },
-  "8": {
-    "base": "6",
-    "value": "20220554335330240002224253"
-  },
-  "9": {
-    "base": "12",
-    "value": "45153788322a1255483"
-  },
-  "10": {
-    "base": "7",
-    "value": "1101613130313526312514143"
-  }
-}
-    """;
+        String filePath = "input.json";
+
+        try {
+            // Read entire file content into a String
+            String jsonString = new String(Files.readAllBytes(Paths.get(filePath)));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
 
         jsonString = jsonString.replace("{", "")
                                .replace("}", "")
@@ -156,4 +121,5 @@ class Main {
         return constant;
     }
 }
+
 
